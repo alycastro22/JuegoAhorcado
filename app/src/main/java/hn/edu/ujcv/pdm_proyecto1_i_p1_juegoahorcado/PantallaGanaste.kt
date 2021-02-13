7071f94c4af32e940e217039932d5b6dce0816b7
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-02-13 00:10:25 -0600

    Pantalla Ganaste

---
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaGanaste.kt
@@ -0,0 +1,12 @@
+package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaGanaste : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_ganaste)
+    }
+
+}
--- app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
@@ -1,11 +1,19 @@
 package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PantallaJuego : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_juego)
     }
+
+    fun ganar(view: View) {
+        val cambiarPantalla = Intent(this, PantallaGanaste::class.java)
+        startActivity(cambiarPantalla)
+    }
+
 }
