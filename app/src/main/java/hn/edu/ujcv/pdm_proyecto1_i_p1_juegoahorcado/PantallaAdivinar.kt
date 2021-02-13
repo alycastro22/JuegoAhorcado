0c1025be0a640e609f73fcddfb35e08681992d01
alycastro22 <allisson.castro@ujcv.edu.hn>
2021-02-13 16:01:51 -0600

    Diseño de pantalla adivinar

---
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaAdivinar.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaAdivinar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_adivinar)
+    }
+}
