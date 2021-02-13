c2653d471502706ae41dbff364a285084d547fab
alycastro22 <allisson.castro@ujcv.edu.hn>
2021-02-12 20:31:55 -0600

    Primer Commit

---
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/MainActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
