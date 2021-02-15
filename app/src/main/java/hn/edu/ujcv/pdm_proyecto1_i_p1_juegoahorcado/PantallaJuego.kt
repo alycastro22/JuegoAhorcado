c3e9191043531f06c720631db59d9959e68532d4
aramel08 <78820331+aramel08@users.noreply.github.com>
2021-02-14 20:34:44 -0600

    NI sirve

--- app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
@@ -2,25 +2,56 @@ package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_pantalla_adivinar.*
 import kotlinx.android.synthetic.main.activity_pantalla_juego.*
 
 class PantallaJuego : AppCompatActivity() {
+
+    //private val charView = Array<TextView>(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_juego)
         mostrarpista()
     }
-    fun mostrarpista(){
-        val lista : ArrayList<String>? = intent.getStringArrayListExtra("Lista")
 
-        if (lista != null) {
-            txtPista.text = getString(R.string.Pista1, lista.random().toString())
-        }
+
+    fun mostrarpista(){
+
+        val lista : ArrayList<String>? = intent.getStringArrayListExtra("Lista")
+        val pista = ArrayList<String>()
+        pista.add("Animal de 8 patas")
+        pista.add("Tiene 7 días")
+        pista.add("Septimo Mes")
+        pista.add("Tiene melodía y ritmo")
+
+        val palabras = ArrayList<String>()
+        palabras.add("ARAÑA")
+        palabras.add("SEMANA")
+        palabras.add("JULIO")
+        palabras.add("CANCION")
+
+        val palabra = mutableMapOf<String,String>("Araña" to "Animal 8 patas", "Semana" to "Tiene 7 días", "Julio" to "Septimo Mes", "Cancion" to "Melodia y ritmo")
+        palabra.put("Animal 8 patas","Araña")
+
+
+        val palabrax = palabra.keys.random()
+        val pistax= palabra.getValue(palabrax)
+        val pistaxx= palabras.random()
+        val palabrajuego=palabrax
+        txtPista.text = getString(R.string.Pista1, pistax)
+
+       val charView= Array<TextView>(palabrax.length)
+
+        for ( i in palabrax)
+            charView[i.toInt()]= TextView(this )
+            charView.
+
+
     }
+
 
     fun ganar(view: View) {
         val cambiarPantalla = Intent(this, PantallaGanaste::class.java)
