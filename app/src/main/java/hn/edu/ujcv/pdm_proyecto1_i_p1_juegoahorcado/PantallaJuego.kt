86619d7167a8e55ec7ad475fa75e79c869f71b95
aramel08 <78820331+aramel08@users.noreply.github.com>
2021-02-15 01:26:12 -0600

    Medio Sirve

--- app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
@@ -1,13 +1,14 @@
 package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_pantalla_juego.*
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -16,15 +17,22 @@ class PantallaJuego : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_juego)
-        mostrarpista()
+        mostrarpista(palabra)
+        btnValidar.setOnClickListener{Validar(palabra)}
+
     }
+    private var palabra = mutableMapOf<String,String>("Araña" to "Animal 8 patas", "Semana" to "Tiene 7 días", "Julio" to "Septimo Mes", "Cancion" to "Melodia y ritmo", "Araña" to "Animal 8 patas ")
+    private val palabrax = palabra.keys.random()
+    private val pistax= palabra.getValue(palabrax)
+    private var intentos = 6
+    private var partecuerpo = 0
 
 
-    fun mostrarpista(){
+
+     fun mostrarpista(mutableMap: MutableMap<String, String>){
         val lista : ArrayList<String>? = intent.getStringArrayListExtra("Lista")
         val pista = ArrayList<String>()
-        var intentos = 6
-        var partecuerpo = 0
+
         pista.add("Animal de 8 patas")
         pista.add("Tiene 7 días")
         pista.add("Septimo Mes")
@@ -36,35 +44,91 @@ class PantallaJuego : AppCompatActivity() {
         palabras.add("JULIO")
         palabras.add("CANCION")
 
-        val palabra = mutableMapOf<String,String>("Araña" to "Animal 8 patas", "Semana" to "Tiene 7 días", "Julio" to "Septimo Mes", "Cancion" to "Melodia y ritmo")
-        palabra.put("Animal 8 patas","Araña")
+        //val palabra = mutableMapOf<String,String>("Araña" to "Animal 8 patas", "Semana" to "Tiene 7 días", "Julio" to "Septimo Mes", "Cancion" to "Melodia y ritmo")
 
-        val palabrax = palabra.keys.random()
-        val pistax= palabra.getValue(palabrax)
-        val pistaxx= palabras.random()
+
+
         txtPista.text = getString(R.string.Pista1, pistax)
 
-        var parte = ArrayList<ImageView>(6)
-        parte = arrayListOf(findViewById(R.id.imageCabeza))
-        parte = arrayListOf(findViewById(R.id.imageCuerpo))
-        parte = arrayListOf(findViewById(R.id.imageBrazoDerecho))
-        parte = arrayListOf(findViewById(R.id.imageBrazoIzquierdo))
-        parte = arrayListOf(findViewById(R.id.imagePiernaDerecha))
-        parte = arrayListOf(findViewById(R.id.imagePiernaIzquierda))
+
+    }
 
 
-        if(txtpalabra.text.toString().equals(palabrax)){
-            val ganar = Intent(this, PantallaGanaste::class.java)
-            startActivity(ganar)
-        }else if (partecuerpo < intentos){
-            parte[intentos].visibility
-            intentos++
+    fun Validar(mutableMap: MutableMap<String, String>) {
+        intentos=intentos-1
+
+        txvIntento.text = getString(R.string.intento, intentos.toString())
+
+        while(intentos!=0){
+            //
+            // intentos=intentos--
+            if (txtpalabra.text.toString().equals(palabrax)) {
+                val ganar = Intent(this, PantallaGanaste::class.java)
+                startActivity(ganar)
+            }
+            if (intentos == 5) {
+            imageCabeza.visibility = View.VISIBLE
+            }
+            if (intentos == 4) {
+            imageCabeza.visibility = View.VISIBLE
+            imageCuerpo.visibility = View.VISIBLE
+            }
+            if (intentos == 3) {
+            imageCabeza.visibility = View.VISIBLE
+            imageCuerpo.visibility = View.VISIBLE
+            imageBrazoDerecho.visibility = View.VISIBLE
+            }
+            if (intentos == 2) {
+            imageCabeza.visibility = View.VISIBLE
+            imageCuerpo.visibility = View.VISIBLE
+            imageBrazoDerecho.visibility = View.VISIBLE
+            imageBrazoIzquierdo.visibility = View.VISIBLE
+            }
+            if (intentos == 1) {
+            imageCabeza.visibility = View.VISIBLE
+            imageCuerpo.visibility = View.VISIBLE
+            imageBrazoDerecho.visibility = View.VISIBLE
+            imageBrazoIzquierdo.visibility = View.VISIBLE
+            imagePiernaDerecha.visibility = View.VISIBLE
+            }
+            if (intentos == 0) {
+            imageCabeza.visibility = View.VISIBLE
+            imageCuerpo.visibility = View.VISIBLE
+            imageBrazoDerecho.visibility = View.VISIBLE
+            imageBrazoIzquierdo.visibility = View.VISIBLE
+            imagePiernaDerecha.visibility = View.VISIBLE
+            imagePiernaIzquierda.visibility = View.VISIBLE
             val perder = Intent(this, PantallaPerdedor::class.java)
             startActivity(perder)
+            }
         }
 
     }
 
+
+
+
+    /* fun validarletra(): Boolean{
+             Pattern p = Pattern.compile("^([A-Z])$");
+             Matcher m = p.matcher(.getText());
+
+
+             if(m.find() && m.group().equals(.getText())){
+                 return true;
+             } else{
+                 Alert alert =new Alert(Alert.AlertType.ERROR);
+                 alert.setTitle("Validar nombre");
+                 alert.setHeaderText(null);
+                 alert.setContentText("Verifique la siguiente información: " +
+                         " \nDeberá escribir un nombre que contenga:" +
+                         " \nPrimera letra mayúscula" +
+                         " \nAl menos un apellido" +
+                         " \nEste campo solo letras");
+                 alert.showAndWait();
+
+                 return false;
+             }
+         }*/
 
 }
 
