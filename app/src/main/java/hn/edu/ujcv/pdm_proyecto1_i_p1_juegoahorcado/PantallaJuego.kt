fe1bacc316c55475c9bcf15c039f00bf3e934565
aramel08 <78820331+aramel08@users.noreply.github.com>
2021-02-17 18:41:54 -0600

    casi casi raza :(

--- app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_proyecto1_i_p1_juegoahorcado/PantallaJuego.kt
@@ -3,13 +3,20 @@ package hn.edu.ujcv.pdm_proyecto1_i_p1_juegoahorcado
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.ArraySet
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_pantalla_juego.*
+import java.sql.Array
+import java.util.*
 
 class PantallaJuego : AppCompatActivity() {
-    var palabra = mutableMapOf<String,String>("araña" to "Animal 8 patas", "semana" to "Tiene 7 días", "julio" to "Septimo Mes", "cancion" to "Melodia y ritmo", "araña" to "Animal 8 patas ",
-            "carro" to "Vehiculo", "perro" to "Mascota", "rojo" to "Color Pasión", "domingo" to "Día de descanso", "luna" to "satelite natural de la tierra", "año" to "tiene 365 días ")
+    var palabra = mutableMapOf<String,String>("araña" to "Animal 8 patas", "semana" to "Tiene 7 días", "julio" to "Septimo Mes", "cancion" to "Melodia y ritmo", "araña" to "Animal 8 patas ")
+            //"carro" to "Vehiculo", "perro" to "Mascota", "rojo" to "Color Pasión", "domingo" to "Día de descanso", "luna" to "satelite natural de la tierra", "año" to "tiene 365 días ")
     private var palabrax = ""
     private var pistax = ""
     private var intentos = 6
@@ -18,6 +25,8 @@ class PantallaJuego : AppCompatActivity() {
     private var numchar = 0
     var correcto = false
     var equivocado =  false
+     var resultado = arrayOfNulls<TextView>(0)
+    // var letraArray
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +35,9 @@ class PantallaJuego : AppCompatActivity() {
         agregarPalabra()
         mostrarpista(palabra)
         btnValidar.setOnClickListener{Validar(palabra)}
-       // muñeco()
+
+        CrearViews()
+        //muñeco()
 
     }
 
@@ -44,52 +55,82 @@ class PantallaJuego : AppCompatActivity() {
 
      fun mostrarpista(mutableMap: MutableMap<String, String>){
          palabrax = palabra.keys.random()
+         resultado = arrayOfNulls(palabrax.length)
+         //CrearViews()
          pistax = palabra.getValue(palabrax)
+         //muñeco()
 
-        txtPista.text = getString(R.string.Pista1, pistax)
+
+        // resultado = palabrax
+
+         txtPista.text = getString(R.string.Pista1, pistax)
 
     }
 
 
     fun Validar(mutableMap: MutableMap<String, String>) {
-
         numchar = palabrax.length
-
-        /*for (palabraChar in palabrax){
-            val palabraChar : CharArray = palabrax.toCharArray()
-            println(palabraChar[0])
-
-        }*/
+    if(txtpalabra.text.isNotEmpty()){
         for (letra in palabrax){
             var letrausaruio = txtpalabra.text.last()
             if (letra.equals(letrausaruio)){
-                correcto = true
-                txtvMostrarPalabra.setText(letra.toString())
                 numcorr++
-            }else{
-                equivocado = true
+                correcto = true
+                break
             }
 
         }
+        if (!correcto ){
+            --intentos
 
-        if (correcto) {
+            partecuerpo++
+            muñeco()
+            limpiar()
+        }else {
+            MostrarLetra()
+            limpiar()
             if (numcorr == numchar) {
                 val ganar = Intent(this, PantallaGanaste::class.java)
                 startActivity(ganar)
                 //partecuerpo--
-                limpiar()
+
             }
-        }else if(equivocado){
-            partecuerpo++
-            println(partecuerpo)
         }
-        muñeco()
+    }else{
+        Toast.makeText(this, "Debe ingresar Letra", Toast.LENGTH_SHORT).show()
+    }
+        //muñeco()
+
+    }
+
+    private fun CrearViews(){
+        val Ilayout:LinearLayout = findViewById<View>(R.id.layoutlinea) as LinearLayout
+        for ( i : Int in palabrax.indices){
+            resultado[i] = TextView(this)
+            resultado[i]?.id = i
+            resultado[i]?.setTextColor(resources.getColor(R.color.white))
+            resultado[i]?.hint= " _ "
+            resultado[i]?.setHintTextColor(resources.getColor(R.color.white))
+            resultado[i]?.textSize= 25F
+            Ilayout.addView(resultado[i])
+            resultado[i]?.isVisible = true
+        }
+    }
+    private fun MostrarLetra(){
+        val str: String = txtpalabra.text.toString().toLowerCase(Locale.ROOT)
+        for ( i:Int in palabrax.indices){
+            if(txtpalabra.text.single().toLowerCase()== palabrax[i]){
+                resultado[i]?.text = " ".plus(str.toUpperCase(Locale.ROOT)).plus(" ")
+
+            }
+
+        }
     }
 
 
     fun muñeco(){
-        if (partecuerpo<=intentos) {
-            if (partecuerpo == 1) {
+        //if (partecuerpo <=intentos) {
+            if (partecuerpo == 1 ) {
                 imageCabeza.visibility = View.VISIBLE
 
             }
@@ -98,7 +139,7 @@ class PantallaJuego : AppCompatActivity() {
                 imageCuerpo.visibility = View.VISIBLE
 
             }
-            if (partecuerpo == 3) {
+             if (partecuerpo == 3) {
                 imageCabeza.visibility = View.VISIBLE
                 imageCuerpo.visibility = View.VISIBLE
                 imageBrazoDerecho.visibility = View.VISIBLE
@@ -111,7 +152,7 @@ class PantallaJuego : AppCompatActivity() {
                 imageBrazoIzquierdo.visibility = View.VISIBLE
 
             }
-            if (partecuerpo == 5) {
+             if (partecuerpo == 5) {
                 imageCabeza.visibility = View.VISIBLE
                 imageCuerpo.visibility = View.VISIBLE
                 imageBrazoDerecho.visibility = View.VISIBLE
@@ -130,7 +171,7 @@ class PantallaJuego : AppCompatActivity() {
                 startActivity(perder)
 
             }
-        }
+
     }
 
     fun limpiar(){
